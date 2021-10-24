c9e3e3e10f21fae1166ff4a0226e60a132c3135f
PREMSAGAR KUSHWAHA <67200446+premsagarkushwaha@users.noreply.github.com>
2021-10-24 11:29:18 +0530

    Reverse The Number Problem Code: FLOW007 update

--- Reverse_The_Number.cpp
+++ Reverse_The_Number.cpp
@@ -1,3 +1,14 @@
+/*  Given an Integer N, write a program to reverse it.
+
+Input
+The first line contains an integer T, total number of testcases. Then follow T lines, each line contains an integer N.
+
+Output
+For each test case, display the reverse of the given number N, in a new line
+*/
+
+
+
 #include <iostream>
 using namespace std;
 
