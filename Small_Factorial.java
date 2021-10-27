6a022f8283c2661c1f2254dc1ce6fbbc497f15cb
PREMSAGAR KUSHWAHA <67200446+premsagarkushwaha@users.noreply.github.com>
2021-10-27 23:37:57 +0530

    update

--- Small_Factorial.java
+++ Small_Factorial.java
@@ -33,6 +33,7 @@ class Codechef
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
+		
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
 		ArrayList<Integer> list = new ArrayList<>();
